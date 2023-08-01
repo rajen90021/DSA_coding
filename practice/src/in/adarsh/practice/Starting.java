69fa8a5ece7670dc26e8b374302a0966079c722b
rajen13 <rajen@kloudone.com>
2023-08-01 12:06:23 +0530

    added dev2 branch

--- practice/src/in/adarsh/practice/Starting.java
+++ practice/src/in/adarsh/practice/Starting.java
@@ -4,5 +4,6 @@ public class Starting {
     public static void main(String[] args) {
         System.out.println("hiiii rajen starting");
         System.out.println("hiiii rajen starting");
+        System.out.println("hiiii rajen starting from dev2 ");
     }
 }
