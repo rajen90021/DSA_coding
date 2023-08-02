a426acd6199f8ca8cafdec41c71ad5723fae1bde
rajen13 <rajen@kloudone.com>
2023-08-02 13:22:18 +0530

    added

--- practice/src/in/adarsh/practice/Starting.java
+++ practice/src/in/adarsh/practice/Starting.java
@@ -5,7 +5,7 @@ public class Starting {
         System.out.println("hiiii rajen starting");
         System.out.println("hiiii rajen starting");
 
-        System.out.println("hiiiiiiii rajeeen startinnnnngdd dev 1..");
+        System.out.println("hiiiiiiii rajeeen ddstartinnnnngdd dev 1..");
 
     }
 }
--- practice/src/in/adarsh/practice/dev2.java
+++
@@ -1,7 +0,0 @@
-package in.adarsh.practice;
-
-public class dev2 {
-    public static void main(String[] args) {
-        System.out.println("hiii");
-    }
-}
