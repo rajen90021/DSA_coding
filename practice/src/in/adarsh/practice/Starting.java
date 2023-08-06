3a2ad163553881516c58cf68953c70f13f731851
rajen13 <rajen@kloudone.com>
2023-08-06 21:30:35 +0530

    added some question

--- practice/src/in/adarsh/practice/Starting.java
+++ practice/src/in/adarsh/practice/Starting.java
@@ -111,18 +111,118 @@ public static  void valueoccurance(int[] arr,int val){
       ans[1]=arr[arr.length-1];
       return  ans;
     }
+    public static void targetsum(int[] arr ,int target) {
+
+//    int count=0;
+//    for (int i=0;i< arr.length;i++){
+//     for(int j=i+1;j<arr.length;j++){
+//         if(arr[i]+arr[j]==target){
+//             count++;
+//         }
+//     }
+//    }
+//        System.out.println(count);
+HashMap<Integer,Integer> map= new HashMap<Integer,Integer>();
+int count=0;
+            for(int value: arr) {
+
+                if(map.containsKey(value)){
+                    int oldfre= map.get(value);
+                    int newfre= oldfre+1;
+                    map.put(value,newfre);
+                }
+                else{
+                    map.put(value,1);
+                }
+            }
+            for(int val2: arr){
+                if(map.containsKey(target-val2)){
+                    count += map.get(target-val2);
+                }
+            }
+        System.out.println(count/2);
+
+}
+public static void uniquenoinrepatingarray(int[] arr){
+
+//    HashMap<Integer,Integer> map= new HashMap<>();
+//    for(int data : arr) {
+//        if (map.containsKey(data)) {
+//            int oldfre = map.get(data);
+//            int newfre = oldfre + 1;
+//            map.put(data, newfre);
+//        } else {
+//            map.put(data, 1);
+//        }
+//    }
+//    for(int getvalue : map.keySet()){
+//        int val = map.get(getvalue);
+//        if(val==1){
+//            System.out.println("unique element is "+getvalue);
+//            break;
+//        }
+//        //System.out.println(getvalue+"  "+val);
+//    }
+//  //  System.out.println(getvalue+"  "+val);
+
+    for(int i=0;i< arr.length;i++){
+
+
+        for(int j=i+1;j<arr.length;j++)
+        {
+            if(arr[i]==arr[j]){
+                arr[i]=-1;
+                arr[j]=-1;
+            }
+
+          }
+        }
+    for(int ans : arr){
+        if(ans>0){
+            System.out.println("ans is "+ans);
+        }
+    }
+}
+public static void secondlargest(int[] arr){
+    int max=Integer.MIN_VALUE;
+    for(int data : arr){
+        if(max<data){
+            max=data;
+        }
+    }
+    System.out.println(max);
+    for (int i= 0;i< arr.length;i++){
+        if(arr[i]==max){
+            arr[i]= -1;
+        }
+    }
+    for (int i=0;i<arr.length;i++){
+        System.out.print(" "+arr[i]);
+    }
+    int max2= Integer.MIN_VALUE;
+    for (int data : arr){
+        if(max2<data){
+            max2=data;
+        }
+    }
+    System.out.println("\n second largest is \n"+max2);
+}
+
     public static void main(String[] args) {
-        int[] arr = {2,5,6,7,8,9,15,29};
+        int[] arr = {3,4,5,19,2,55,91,9};
 
       //  largestelement(arr);
       //  int value = searchelement(arr, 0);
        // System.out.println("value is "+ value);
         //valueoccurance(arr,25);
-        //lastoccurane(arr,25);
-        System.out.println("array is sorted "+issorted(arr));
-        int[] ans2  =   returnmaxandmin(arr);
-        for (int i=0;i<ans2.length;i++){
-            System.out.println(""+ans2[i]);
-        }
-    }
+//        //lastoccurane(arr,25);
+//        System.out.println("array is sorted "+issorted(arr));
+//        int[] ans2  =   returnmaxandmin(arr);
+//        for (int i=0;i<ans2.length;i++){
+//            System.out.println(""+ans2[i]);
+        //targetsum(arr, 2);
+//        uniquenoinrepatingarray(arr);
+        //secondlargest(arr);
 }
+    }
+
